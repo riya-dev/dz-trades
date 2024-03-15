2f4a84c2bbede98fec1bd8bb4ad1a9c0a71f3d64
Riya Dev <riyasdev9@gmail.com>
2024-03-15 01:54:47 -0400

    Clean up, print statement.

--- alg.py
+++ alg.py
@@ -1,9 +1,9 @@
-import numpy as np
 import math
 from scipy.stats import norm
 
 def black_scholes(S_t, K, r, t, sigma):
     """ black_scholes algorithm """
+    # C = call option price
     # S_t = spot price
     # K = strike price
     # r = risk-free interest rate
@@ -18,8 +18,8 @@ def black_scholes(S_t, K, r, t, sigma):
     d2 = d1 - sigma * math.sqrt(t)
     C = norm.cdf(d1) * S_t - norm.cdf(d2) * K * math.e**(-r * t)
     return C
-    
+
 
 t_m = 3/365
 b_s = black_scholes(20, 18, 0.04, t_m, 0.5)
-print(b_s)
+print("black scholes call option price: ", b_s)
